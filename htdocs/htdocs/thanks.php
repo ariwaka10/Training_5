82a9fb2048180a96785a4a833f9496285faaa1dc
ariwaka10 <insight.marke@gmail.com>
2025-03-30 09:02:44 +0900

    Delete htdocs directory

--- htdocs/htdocs/thanks.php
+++
@@ -1,9 +0,0 @@
-<?php include(dirname(__FILE__).'/header.php'); ?>
-
-
-<div class="thanks">
-    <p class="thanks_message">お問い合わせありがとうございました</p> 
-</div>
-
-
-<?php include(dirname(__FILE__).'/footer.php'); ?>
